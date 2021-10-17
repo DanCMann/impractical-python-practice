ebf5b99ed581407e1ad47786baea1cf9b8c855f7
DanCMann <danmann23@gmail.com>
2021-10-17 11:36:09 +0200

    removes my version of palingram check and replaces with book code since it's more efficient.

--- scripts/palingrams.py
+++ scripts/palingrams.py
@@ -8,39 +8,17 @@ import load_dictionary
 def find_palingrams(wordlist):
 
     palingrams = []
-    palin_list = []
 
-    count = 0
     for word in wordlist:
-
-        if count % 500 == 0:
-            print("MY LIST: {}\nBOOK LIST: {}\n\n".format(palingrams, palin_list))
-            print("MY LIST: {}\nBOOK LIST: {}".format(len(palingrams), len(palin_list)))
-
-        count += 1
 
         end = len(word)
         rev_word = word[::-1]
 
         for i in range(end):
             if word[i:] == rev_word[:end-i] and rev_word[end-i:] in wordlist: 
-                palin_list.append((word, rev_word[end-i:]))
+                palingrams.append((word, rev_word[end-i:]))
             if word[:i] == rev_word[end-i:] and rev_word[:end-i] in wordlist:
-                palin_list.append((rev_word[:end-i], word))
-
-        for i in range(len(word)):
-            splice = word[i::-1]
-            revsplice = word[i:][::-1]
-
-            if splice in wordlist:
-                palin = word + splice
-                if palin == palin[::-1]:
-                    palingrams.append(word + " " + splice)
-
-            if revsplice in wordlist:
-                palin = revsplice + word
-                if palin == palin[::-1]:
-                    palingrams.append(revsplice + " " + word)
+                palingrams.append((rev_word[:end-i], word))
 
     return palingrams
 
@@ -49,3 +27,4 @@ if __name__ == '__main__':
     file = "data/2of4brif.txt"
     words = load_dictionary.load(file)
     palingrams = find_palingrams(words)
+    print(palingrams)
