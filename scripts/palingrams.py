3f425dd6b48c22b2ca3e208392d74aa6fbf3d5ef
DanCMann <danmann23@gmail.com>
2021-10-17 11:26:53 +0200

    adds approaches that book uses and a check to see if I get the same results.

--- scripts/palingrams.py
+++ scripts/palingrams.py
@@ -8,8 +8,25 @@ import load_dictionary
 def find_palingrams(wordlist):
 
     palingrams = []
+    palin_list = []
 
+    count = 0
     for word in wordlist:
+
+        if count % 500 == 0:
+            print("MY LIST: {}\nBOOK LIST: {}\n\n".format(palingrams, palin_list))
+            print("MY LIST: {}\nBOOK LIST: {}".format(len(palingrams), len(palin_list)))
+
+        count += 1
+
+        end = len(word)
+        rev_word = word[::-1]
+
+        for i in range(end):
+            if word[i:] == rev_word[:end-i] and rev_word[end-i:] in wordlist: 
+                palin_list.append((word, rev_word[end-i:]))
+            if word[:i] == rev_word[end-i:] and rev_word[:end-i] in wordlist:
+                palin_list.append((rev_word[:end-i], word))
 
         for i in range(len(word)):
             splice = word[i::-1]
