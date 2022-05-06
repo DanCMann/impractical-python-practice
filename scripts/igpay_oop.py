23e278c5c85117ff48245b3e2e7f9d9ec7ae2742
Dan C. Mann <danmann23@gmail.com>
2022-05-06 17:56:52 +0200

    adds pig lating game done in more oop style.

---
+++ scripts/igpay_oop.py
@@ -0,0 +1,55 @@
+# Pig Latin
+# Date: 2021-09-25
+# Task: "Write a program that takes a word as input and uses indexing
+#   and slicing to return its Pig Latin equivalent."
+
+from Game import Game
+
+class Main():
+    """ Main function for running pig latin game.
+    """
+    def __init__(self):
+        game = PigLatinGame()
+        game.run()
+
+class PigLatinGame(Game):
+    """Pig Latin game class.
+    """
+    def __init__(self):
+        super().__init__()
+        print("Translate into IgPay AtinLay!!\n")
+        self.string_input = input("What word do you want translated? ")
+
+    def game_loop(self):
+            print("\t{}".format(self.pig_latin(self.string_input)))
+            self.string_input = input("Enter another word to keep playing? ('n' to end game) ")
+            if self.string_input.lower() == 'n':
+                self.end_game()
+
+    def pig_latin(self, string):
+        """ Translates an English word into Pig Latin.
+            Takes all consonants before the first vowel then moves them to the end of the string,
+            then adds -ay to the end.
+
+        Args:
+            string (str): String to be converted into Pig Latin.
+
+        Returns:
+            str: Pig Latin conversion of string.
+        """
+        vowels = "aeiou"
+        string = string.lower()
+        count = 0
+        for letter in string:
+            if letter in vowels:
+                pig = string[:count] + 'ay'
+                break
+            if letter == 'y' and count != 0:
+                pig = string[:count] + 'ay'
+                break
+            count += 1
+
+        return string[count:] + pig
+
+if __name__ == '__main__':
+    Main()
